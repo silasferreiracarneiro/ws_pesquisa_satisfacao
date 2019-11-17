14fa25cb877aef9b00b9ee69f6e23bece5284752
silasferreiracarneiro <silasferreira96@hotmail.com>
2019-11-17 14:40:04 -0300

    first commit

---
+++ src/main/java/com/fiap/PesquisaSatisfacao/PesquisaSatisfacaoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.PesquisaSatisfacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaSatisfacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaSatisfacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/PesquisaSatisfacao/PesquisaSatisfacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.PesquisaSatisfacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaSatisfacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
